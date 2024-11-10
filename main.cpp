d45d59d30dc51346118853dadbf4bcb97d5941fa
Nguyen Huy Hieu <hieu26.nh@gmail.com>
2024-11-11 02:19:58 +0700

    aaaaaaaaaaaaa

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <sstream>
 #include <vector>
 #include <queue>
+#include <unordered_map>
 
 using namespace std;
 
@@ -270,42 +271,41 @@ int getPriority(string op) {
 }
 
 int calculate(string s) {
-    stack<int> numStk;
-    stack<string> operatorStk;
-    string currentNum = "";
+    stack<int> num;
+    stack<char> op;
+    int currentNum = 0;
     for (char c: s) {
         if (c == ' ') continue;
-        string str = string(1,c);
-        if (isOperator(str)) {
-            if (currentNum.length() != 0) {
-                numStk.push(stoi(currentNum));
-                currentNum = "";
-            }
-            while (!operatorStk.empty() && getPriority(operatorStk.top()) >= getPriority(str)) {
-                string opTop = operatorStk.top();
-                operatorStk.pop();
-                int num1 = numStk.top();
-                numStk.pop();
-                int num2 = numStk.top();
-                numStk.pop();
-                numStk.push(calculator(num1, num2, opTop));
-            }
-            operatorStk.push(str);
+        if (isdigit(c)) {
+            currentNum = currentNum * 10 + (c - '0');
         } else {
-            currentNum += str;
+            if (currentNum != 0) {
+                num.push(currentNum);
+                currentNum = 0;
+            }
+            while (!op.empty() && getPriority(op.top()) >= getPriority(c)) {
+                char _operator = op.top();
+                op.pop();
+                int num1 = num.top();
+                num.pop();
+                int num2 = num.top();
+                num.pop();
+                num.push(calculator(num2, num1, _operator));
+            }
+            op.push(c);
         }
     }
-    if (currentNum.length() != 0) numStk.push(stoi(currentNum));
-    while (numStk.size() != 1) {
-        int num1 = numStk.top();
-        numStk.pop();
-        int num2 = numStk.top();
-        numStk.pop();
-        string op = operatorStk.top();
-        operatorStk.pop();
-        numStk.push(calculator(num1, num2, op));
+    if (currentNum != 0) num.push(currentNum);
+    while (!op.empty()) {
+        char _operator = op.top();
+        op.pop();
+        int num1 = num.top();
+        num.pop();
+        int num2 = num.top();
+        num.pop();
+        num.push(calculator(num2, num1, _operator));
     }
-    return numStk.top();
+    return num.top();
 }
 
 class MyQueue {
@@ -398,11 +398,311 @@ string removeDuplicateLetters(string s) {
     return ans;
 }
 
+class NestedIterator {
+private:
+    stack<NestedInteger> stk1;
+    stack<int> stk2;
+    void flat(vector<NestedInteger> &nestedList) {
+        for (NestedInteger nested: nestedList) {
+            stk1.push(nested);
+        }
+        while (!stk1.empty()) {
+            NestedInteger top = stk1.top();
+            stk1.pop();
+            if (top.isInteger()) {
+                stk2.push(top.getInteger());
+            } else {
+                flat(top.getList());
+            }
+        }
+    }
+public:
+    NestedIterator(vector<NestedInteger> &nestedList) {
+        flat(nestedList);
+    }
+    int next() {
+        int result = stk2.top();
+        stk2.pop();
+        return result;
+    }
+    bool hasNext() {
+        return !stk2.empty();
+    }
+};
+
+string decodeString(string s) {
+  stack<int> num;
+  stack<string> str;
+  int currentNum = 0;
+  string currentStr = "";
+  for (char c: s) {
+    if (isdigit(c)) {
+      currentNum = currentNum * 10 + (c - '0');
+    } else {
+      if (c == '[') {
+        num.push(currentNum);
+	currentNum = 0;
+	str.push(currentStr);
+	currentStr = "";
+      } else if (c == ']') {
+	int numTop = num.top();
+	num.pop();
+	string strTop = str.top();
+	str.pop();
+	for (int i=0; i<numTop; i++) {
+	  strTop += currentStr;
+	}
+	currentStr = strTop;
+      } else {
+        currentStr += c;
+      }
+    }
+  }
+  return currentStr;
+}
+
+string removeKdigits(string num, int k) {
+	stack<char> s;
+	for (char c: num) {
+		while (!s.empty() && s.top() > c && k > 0) {
+			s.pop();
+			k --;
+		}
+		s.push(c);
+	}
+	while (k > 0) {
+		s.pop();
+		k --;
+	}
+	string ans = "";
+	while (!s.empty()) {
+		ans += s.top();
+		s.pop();
+	}
+	reverse(ans.begin(), ans.end());
+	int idx = 0;
+	while (idx < ans.size() && ans[idx] == '0') {
+		idx ++;
+	}
+	ans = ans.substr(idx);
+	if (ans == "") return "0";
+	return ans;
+}
+
+ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+	stack<int> stk;
+	stack<int> stk1;
+	stack<int> stk2;
+	ListNode* dummy = new ListNode(0);
+	int carry = 0;
+	while (l1 != nullptr || l2 != nullptr) {
+		if (l1 != nullptr) {
+			stk1.push(l1->val);
+			l1 = l1->next;
+		}
+		if (l2 != nullptr) {
+			stk2.push(l2->val);
+			l2 = l2->next;
+		}
+	}
+	while (!stk1.empty() || !stk2.empty()) {
+		int sum = carry;
+		if (!stk1.empty()) {
+			sum += stk1.top();
+			stk1.pop();
+		}
+		if (!stk2.empty()) {
+			sum += stk2.top();
+			stk2.pop();
+		}
+		stk.push(sum % 10);
+		carry = sum / 10;
+	}
+	if (carry != 0) stk.push(carry);
+	ListNode* current = dummy;
+	while (!stk.empty()) {
+		current->next = new ListNode(stk.top());
+		stk.pop();
+		current = current->next;
+	}
+	ListNode* ans = dummy->next;
+	delete dummy;
+	return ans;
+}
+
+vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+	unordered_map<int, int> m;
+	stack<int> s;
+	vector<int> ans;
+	for (int num: nums2) {
+		while (!s.empty() && s.top() < num) {
+			m[s.top()] = n;
+			s.pop();
+		}
+		s.push(num);
+	}
+	while (!s.empty()) {
+		m[s.top()] = -1;
+		s.pop();
+	}
+	for (int num: nums1) {
+		ans.push_back(m[num]);
+	}
+	return ans;
+}
+
+vector<int> nextGreaterElements(vector<int>& nums) {
+	int size = nums.size();
+	vector<int> ans(size, -1);
+	stack<int> s;
+	for (int i=0; i<size*2; i++) {
+		int idx = i % size;
+		int num = nums[idx];
+		while (!s.empty() && nums[s.top()] < num) {
+			ans[s.top()] = num;
+			s.pop();
+		}
+		if (i < size) s.push(i);
+	}
+	return ans;
+}
+
+vector<int> preorder(Node* root) {
+	vector<int> ans;
+	if (root == nullptr) return ans;
+	stack<Node*> s;
+	s.push(root);
+	while (!s.empty()) {
+		Node* top = s.top();
+		s.pop();
+		ans.push_back(top->val);
+		int childrens = top->children.size();
+		for (int i=childrens-1; i>= 0; i--) {
+			s.push(top->children[i]);
+		}
+	}
+	return ans;
+}
+
+vector<int> postorder(Node* root) {
+	vector<int> ans;
+	if (root == nullptr) return ans;
+	stack<Node*> s1;
+	stack<Node*> s2;
+	s1.push(root);
+	while (!s1.empty()) {
+		Node* top = s1.top();
+		s1.pop();
+		s2.push(top);
+		for (Node* children: top->children) {
+			s1.push(children);
+		}
+	}
+	while (!s2.empty()) {
+		ans.push_back(s2.top()->val);
+		s2.pop();
+	}
+	return ans;
+}
+
+bool checkValidString(string s) {
+	stack<char> open;
+	stack<char> star;
+	for (int i=0; i<s.length(); i++) {
+		char c = s[i];
+		if (c == '(') {
+			open.push(i);
+		} else if (c == '*') {
+			star.push(i);
+		} else {
+			if (!open.empty()) {
+				open.pop();
+			} else if (!star.empty()) {
+				star.pop();
+			} else {
+				return false;
+			}
+		}
+	}
+	while (!open.empty() && !star.empty()) {
+		if (open.top() < star.top()) {
+			open.pop();
+			star.pop();
+		} else {
+			return false;
+		}
+	}
+	return open.empty();
+}
+
+int calPoints(vector<string>& operations) {
+    std::stack<int> s;
+    int totalPoint = 0;
+    for (std::string c: operations) {
+        if (c == "C" && !s.empty()) {
+            s.pop();
+        } else if (c == "D" && !s.empty()) {
+            int top = s.top();
+            s.push(top * 2);
+        } else if (c == "+" && !s.empty()) {
+            int top1 = s.top();
+            s.pop();
+            int top2 = s.top();
+            s.push(top1);
+            s.push(top2 + top1);
+        } else {
+            s.push(std::stoi(c));
+        }
+    }
+    while (!s.empty()) {
+        totalPoint += s.top();
+        s.pop();
+    }
+    return totalPoint;
+}
+
+string util(string str) {
+	stack<char> s;
+	string ans = "";
+	for (char c: str) {
+		if (c == '#') {
+			if (!s.empty()) s.pop();
+		} else {
+			s.push(c);
+		}
+	}
+	while (!s.empty()) {
+		ans += s.top();
+		s.pop();
+	}
+	return ans;
+}
+
+bool backspaceCompare(string s, string t) {
+	return util(s) == util(t);
+}
+
+TreeNode* increasingBST(TreeNode* root) {
+    TreeNode* res = new TreeNode(0);
+    TreeNode* resCurrent = res;
+    std::stack<TreeNode*> s;
+    TreeNode* current = root;
+    while (!s.empty() || current != nullptr) {
+        while (current != nullptr) {
+            s.push(current);
+            current = current->left;
+        }
+        TreeNode* top = s.top();
+        s.pop();
+        resCurrent->right = new TreeNode(top->val);
+        resCurrent = resCurrent->right;
+        current = top->right;
+    }
+    return res->right;
+}
 
 int main() {
-//    string res = removeDuplicateLetters("cbacdcbc");
-    cout<<res;
+    unordered_map<int, int> myMap;
 
     return 0;
 }
-
